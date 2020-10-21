c4d3067296e749522f62d3505d5738eb5f83aa74
Brian <mcd.briand@gmail.com>
2020-10-20 22:45:50 -0600

    trying some other stuff

--- auth.js
+++ auth.js
@@ -1,9 +1,13 @@
 function authorize() {
-    const clientId = "c1e158e094a144d0b07143e38e0128cf";
-    const scopes = "ugc-image-upload user-read-recently-played user-read-playback-position user-top-read playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public streaming app-remote-control user-read-email user-read-private user-follow-read user-follow-modify user-library-modify user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state";
-    const redirectUri = "http://tagify.mcdbriand.com/oauth.html"
-    const spotifyUrl = "https://accounts.spotify.com";
-    const authUrl = `${spotifyUrl}/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`
-
-    window.location = authUrl;
+    if (sessionStorage.getItem("token")) {
+        window.location = "library.html"
+    } else {
+        const clientId = "c1e158e094a144d0b07143e38e0128cf";
+        const scopes = "ugc-image-upload user-read-recently-played user-read-playback-position user-top-read playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public streaming app-remote-control user-read-email user-read-private user-follow-read user-follow-modify user-library-modify user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state";
+        const redirectUri = "https://tagify.mcdbriand.com/oauth.html"
+        const spotifyUrl = "https://accounts.spotify.com";
+        const authUrl = `${spotifyUrl}/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`
+    
+        window.location = authUrl;
+    }
 }
