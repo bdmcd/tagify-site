b08bd4a410a01a50bc87a9fbbe9e768b7b98fb5b
Brian <mcd.briand@gmail.com>
2020-10-20 22:23:20 -0600

    Download spotify library

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authorize() {
     const clientId = "c1e158e094a144d0b07143e38e0128cf";
-    const scopes = "user-library-read";
+    const scopes = "ugc-image-upload user-read-recently-played user-read-playback-position user-top-read playlist-modify-private playlist-read-collaborative playlist-read-private playlist-modify-public streaming app-remote-control user-read-email user-read-private user-follow-read user-follow-modify user-library-modify user-library-read user-read-currently-playing user-read-playback-state user-modify-playback-state";
     const redirectUri = "http://localhost:8000/oauth.html"
     const spotifyUrl = "https://accounts.spotify.com";
     const authUrl = `${spotifyUrl}/authorize?response_type=code&client_id=${clientId}&scope=${scopes}&redirect_uri=${redirectUri}`
